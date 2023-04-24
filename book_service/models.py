5cc72399c0f7d7b53d2ba2c230384135535f24d1
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-04-24 18:59:49 +0300

    commit before pull

--- book_service/models.py
+++ book_service/models.py
@@ -7,7 +7,7 @@ class Book(models.Model):
         SOFT = "SOFT"
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    cover = models.CharField(max_length=255, choices=CoverType.choices())
+    cover = models.CharField(max_length=255, choices=CoverType.choices)
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
 
