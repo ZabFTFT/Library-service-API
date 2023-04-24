6fb82d645eb2af01a32f1d28f840187c686709ae
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-24 22:09:48 +0300

    Added view for customer password changing

--- customers_service/views.py
+++ customers_service/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-# Create your views here.
+
+from customers_service.serializers import (
+    CustomerManageSerializer,
+    AuthTokenSerializer,
+    CustomerCreateSerializer,
+    CustomerChangePasswordSerializer,
+)
+
+
+class CreateCustomerView(generics.CreateAPIView):
+    serializer_class = CustomerCreateSerializer
+
+
+class ManageCustomerView(generics.RetrieveUpdateAPIView):
+    serializer_class = CustomerManageSerializer
+
+    def get_object(self):
+        return self.request.user
+
+
+class ChangeCustomerPasswordView(generics.UpdateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = CustomerChangePasswordSerializer
+
+    def get_object(self):
+        return self.request.user
+
+
+class CreateTokenView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    serializer_class = AuthTokenSerializer
