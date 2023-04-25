5331ddcbbf1dfb0506621dd9de30dfcf86af2662
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-25 15:45:29 +0300

    Deleted redundant application

---
+++ notification/notifications.py
@@ -0,0 +1,21 @@
+import os
+import asyncio
+
+import telegram
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
+
+
+async def send_notification(message_text: str) -> None:
+    await bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message_text)
+
+
+if __name__ == "__main__":
+    message = "New borrowing created."
+    asyncio.run(send_notification(message))
