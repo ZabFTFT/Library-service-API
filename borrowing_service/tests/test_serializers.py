d815901ef5476a07aad61d4e28620c4486451b09
Artur Tytomyr <art.tytomyr@gmail.com>
2023-04-25 16:53:37 +0300

    added tests for borrowing-service

--- borrowing_service/tests/test_serializers.py
+++ borrowing_service/tests/test_serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.db import IntegrityError
 from django.urls import reverse
 from django.utils import timezone
 from rest_framework import status
@@ -10,54 +9,10 @@ from rest_framework.test import APIClient
 from borrowing_service.models import Borrowing, Book
 from borrowing_service.serializers import BorrowingCreateSerializer
 
-BORROWINGS_URL_LIST = reverse("borrowing_service:borrowing-list")
-BORROWINGS_URL_DETAIL = reverse("borrowing_service:borrowing-detail", args=[1])
-
-
-class BorrowingModelTests(TestCase):
-    def setUp(self):
-        self.book = Book.objects.create(
-            title="Test Book",
-            author="Joane Rowling",
-            cover="HARD",
-            inventory=10,
-            daily_fee=0.50,
-        )
-        self.customer = get_user_model().objects.create_user(
-            email="testuser@example.com", password="testpass"
-        )
-
-    def test_borrow_date_before_expected_return_date(self):
-        borrowing = Borrowing(
-            book=self.book,
-            customer=self.customer,
-            borrow_date=timezone.now(),
-            expected_return_date=timezone.now() - timezone.timedelta(days=1),
-            actual_return_date=timezone.now(),
-        )
-        with self.assertRaises(IntegrityError):
-            borrowing.save()
-
-    def test_expected_return_date_after_borrow_date(self):
-        borrowing = Borrowing(
-            book=self.book,
-            customer=self.customer,
-            borrow_date=timezone.now(),
-            expected_return_date=timezone.now() - timezone.timedelta(days=1),
-        )
-        with self.assertRaises(IntegrityError):
-            borrowing.save()
-
-    def test_actual_return_date_after_borrow_date(self):
-        borrowing = Borrowing(
-            book=self.book,
-            customer=self.customer,
-            borrow_date=timezone.now(),
-            expected_return_date=timezone.now() + timezone.timedelta(days=1),
-            actual_return_date=timezone.now() - timezone.timedelta(days=1),
-        )
-        with self.assertRaises(IntegrityError):
-            borrowing.save()
+BORROWINGS_URL_LIST = reverse("borrowing_service:borrowings-list")
+BORROWINGS_URL_DETAIL = reverse(
+    "borrowing_service:borrowings-detail", args=[1]
+)
 
 
 def borrowing_creating(book, customer):
